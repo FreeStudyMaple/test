b853c3d0a85abb5c2e966be1e1239851025d968f
feng3045034 <519762170@qq.com>
2017-02-09 00:39:15 +0800

    测试1

---
+++ pratice/src/Demo1.java
@@ -0,0 +1,16 @@
+/**
+ * Created by peng3045034 on 2017/2/9.
+ *
+ * 练习1
+ */
+public class Demo1 {
+
+    byte param1;
+    short param2;
+    int param3;
+    long param4;
+    float param5;
+    double param6;
+    char param7;
+    boolean param8;
+}
